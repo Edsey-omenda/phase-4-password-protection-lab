6f78942d26a3503147c344260f0a405c654c163f
Edsey omenda <omendajames0317@gmail.com>
2022-10-06 23:22:22 +0300

    commit tree 94fbca87794b4831fe314758b333bd40b1741c2a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class  UsersController < ApplicationController
       private
     
       def user_params
-        params.permit(:username, :password, :password_confirmation, session[:user_id])
+        params.permit(:username, :password, :password_confirmation)
       end
 
     
